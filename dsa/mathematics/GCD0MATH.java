5ac88ee9b5a4c0f913645cfeb9dd44bdae3505ed
Vishwajeet Behera <Vishwajeetbehera0@gmail.com>
2020-12-01 09:15:35 +0530

    Naive GCD appraoch Added

---
+++ dsa/mathematics/GCD0MATH.java
@@ -0,0 +1,32 @@
+package dsa.mathematics;
+
+import java.util.Scanner;
+
+public class GCD0MATH {
+
+    /*T.C will be O(m), where m is minimum of both number.
+     Basic approach is to find minimum of two numbers and 
+     then run a loop to find out the number that divide both numbers
+     if we find that number break and return unless keep decreasing
+     and break on 1 */
+    public static int naiveGCD(int num1,int num2) {
+       int result = Math.min(num1, num2);
+       while(result>0){
+           if(num1%result==0&&num1%result==0){
+               break;
+           }
+           result--;
+       }
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the two number to find out");
+        int num1=input.nextInt();
+        int num2=input.nextInt();
+        int gcd = naiveGCD(num1,num2);
+        System.out.println("GCD is: "+gcd);
+        input.close();
+    }
+    
+}
