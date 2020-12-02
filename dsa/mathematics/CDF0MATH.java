372db0cb37079e847d869e3b0fa67e11a65f3d0f
Vishwajeet Behera <Vishwajeetbehera0@gmail.com>
2020-12-02 10:30:22 +0530

    Two Program Added

---
+++ dsa/mathematics/CDF0MATH.java
@@ -0,0 +1,44 @@
+package dsa.mathematics;
+
+import java.util.Scanner;
+
+/*Conecpt in this Algo is
+    We know,
+    log(a*b) = log(a) + log(b)
+
+    Therefore
+    log( n! ) = log(1*2*3....... * n) = log(1) + log(2) + ........ +log(n)
+
+    Now, observe that the floor value of log base 
+    10 increased by 1, of any number, gives the
+    number of digits present in that number.
+
+    Hence, output would be : floor(log(n!)) + 1.*/
+
+public class CDF0MATH {
+
+    public static int digitCountFactorial(int number) {
+        if(number<0){
+           return 0;
+        }
+        if(number<=1){
+            return 1;
+        }
+        double digits=0;
+        for(int i=2;i<=number;i++){
+            digits+=Math.log10(i);
+        }
+        return (int)(Math.floor(digits))+1;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the Number To Find Number of Digits in it's factorial: ");
+        int number = input.nextInt();
+        //int size = numberSize(number);
+        //int size = numberSizeLog(number);
+        int size = digitCountFactorial(number);
+        System.out.println("No of Digits in Factorial of Given Number is: "+size);
+        input.close();
+    }
+    
+}
--- dsa/mathematics/LCF0MATH.java
+++ dsa/mathematics/LCF0MATH.java
@@ -1,5 +1,28 @@
 package dsa.mathematics;
 
+import java.util.Scanner;
+
 public class LCF0MATH {
-    
+
+    public static int eucledianGCD(int num1,int num2) {
+        if(num2==0){
+            return num1;
+        }else{
+            return eucledianGCD(num2, num1%num2);
+        }          
+    }
+
+    public static int LCMusingGCD(int a, int b)
+    {
+        return (a / eucledianGCD(a, b)) * b;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the two number to find out LCM");
+        int num1=input.nextInt();
+        int num2=input.nextInt();
+        int gcd = LCMusingGCD(num1,num2);
+        System.out.println("LCM is: "+gcd);
+        input.close();
+    }
 }
