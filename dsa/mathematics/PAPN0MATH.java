42c2385eeb61132da0f27b8419eee0f5b1f2540d
Vishwajeet Behera <Vishwajeetbehera0@gmail.com>
2021-01-02 21:09:52 +0530

    Sieve of Eratosthenes (Program to find all prime numbers below given number) Added

---
+++ dsa/mathematics/PAPN0MATH.java
@@ -0,0 +1,42 @@
+package dsa.mathematics;
+
+import java.util.Scanner;
+
+public class PAPN0MATH {
+
+    public static void allPrimeSieveOfEratosthenes(int number){
+          boolean[] prime = new boolean[number+1];
+          //array initialize to true
+          for(int i=0;i<prime.length;i++){
+              prime[i]=true;
+          }
+          //main loop which increase value of i so we can falseify all multiples of i
+          for(int i=2;i*i<=number;i++){
+             if(prime[i]==true){
+                 //this loop will falseify all the multiple of i starting form 4
+                 for(int j=i*i;j<=number;j=j+i){
+                     prime[j]=false;
+                 }
+             }
+          }
+          //This loop wil print all the primes 
+          System.out.println("All the prime numbers below given number are: ");
+          for(int i=2;i<prime.length;i++){            
+            if(prime[i]==true){
+                System.out.print(i+" ");
+            }
+         }
+          
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the Number To Print all Divisors: ");
+        int number = input.nextInt();
+        allPrimeSieveOfEratosthenes(number);       
+        
+        input.close();
+    }
+
+    
+}
