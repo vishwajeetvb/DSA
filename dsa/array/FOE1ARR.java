a4ddbb516c3abebfabacac9b1e3f433360ddc321
Vishwajeet Behera <Vishwajeetbehera0@gmail.com>
2021-01-04 15:46:41 +0530

    Program to find frequencies of element in an array is added

---
+++ dsa/array/FOE1ARR.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class FOE1ARR {
+    public static void frequency(int[] arr, int size) {
+        int count=1;
+        int i=1;
+        for(;i<size;){
+            while(i<size&&arr[i]==arr[i-1]){
+                count++;
+                i++;
+            } 
+            System.out.println("Frequency of element: "+arr[i-1]+" is "+count); 
+            i++;
+            count=1;           
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int size = input.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the array to find frequency of all element");
+        for(int i=0;i<size;i++){
+            arr[i]=input.nextInt();
+        }
+        frequency(arr, size);
+        
+        input.close();
+    }
+}
