ae4dc3b9fbc0ee50c809906f06473c095e495a0f
Vishwajeet Behera <Vishwajeetbehera0@gmail.com>
2021-01-25 11:33:44 +0530

    Maximum product subarray basic solution added

---
+++ dsa/array/MPSA1ARR.java
@@ -0,0 +1,49 @@
+//This algorithm does'nt cover all cases
+
+import java.util.Scanner;
+
+public class MPSA1ARR {
+    static long maxProduct(int[] arr, int n) {
+        // code here
+        
+        if(arr.length==1){
+            return arr[0];
+        }
+        
+        int product = arr[0]*arr[1];
+        int max_product=product;
+        for(int i=2;i<n;){
+            if(product*arr[i]>arr[i]){
+                if(product*arr[i]==0){
+                    product=arr[i];
+                }else{
+                product*=arr[i];}
+                if(product>max_product)
+                {max_product=product;}
+                
+                i++;
+            }else{
+                product=arr[i];
+                i++;
+            }
+        }
+        return max_product;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of array1: ");
+        int size = input.nextInt();
+        int[] arr = new int[size];
+        for(int i=0;i<size;i++){
+            arr[i]=input.nextInt();
+        }
+        
+        long element = maxProduct(arr,size);  
+        
+        System.out.print("The product is: "+element);
+    
+    
+
+        input.close();
+    }
+}
