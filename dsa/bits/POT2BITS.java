4c47435c639a0af18a10f2c6a80ec228489b2fed
Vishwajeet Behera <Vishwajeetbehera0@gmail.com>
2021-01-28 19:24:08 +0530

    Program to Find Whether Number is power of 2 or not Added

---
+++ dsa/bits/POT2BITS.java
@@ -0,0 +1,29 @@
+/* Here the idea is Biran's Algo
+   which says to find if one set bits is present just by x-1 which will just toggle thenn element means if
+   its 8 = 1000 then 7 = 0111 and then if we do x&(x-1) it will result in 0
+
+   It's T.C is O(1)
+*/
+
+import java.util.Scanner;
+
+public class POT2BITS {
+    public static boolean isPowerofTwo(long x){
+        if(x==0){
+            return false;
+        }
+        if((x&(x-1))==0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the Number: ");
+        long number = input.nextLong();        
+        boolean place = isPowerofTwo(number);
+        System.out.println("Number is Power of Two: "+place);
+        input.close();
+    }
+}
