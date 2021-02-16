c938594ef9f6d366dfadaf9196ce57722f9b1ab2
Vishwajeet Behera <Vishwajeetbehera0@gmail.com>
2021-02-16 17:51:43 +0530

    Program to find inversion added

---
+++ dsa/searchingAndSorting/IC5SAS.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+class IC5SAS{
+    static long inversionCount(long arr[], long N)
+    {
+        return countInv(arr,0,N-1);
+    }
+    static long countInv(long arr[], long l, long r)
+    {
+        long res = 0;
+        if (l<r) {
+           
+            long m = (r + l) / 2;
+     
+            res += countInv(arr, l, m);
+            res += countInv(arr, m + 1, r);
+            res += countAndMerge(arr, l, m , r);
+        }
+        return res;
+    }
+    
+    static long countAndMerge(long arr[], long l, long m, long r)
+    {
+        long n1=m-l+1, n2=r-m;
+        long[] left=new long[(int)n1];long[] right=new long[(int)n2];
+        for(int i=0;i<n1;i++)
+            left[i]=arr[(int)(i+l)];
+        for(int j=0;j<n2;j++)
+            right[j]=arr[(int)(m+1+j)];    
+        long res=0;
+        int i=0,j=0,k=(int)l;
+        while(i<n1 && j<n2){
+            if(left[i]<=right[j])
+                {arr[k++]=left[i++];}
+            else{
+                arr[k++]=right[j++];
+                res=res+((int)(n1-i));
+            }
+        }
+        while(i<n1)
+            arr[k++]=left[i++];
+        while(j<n2)
+            arr[k++]=right[j++];
+        return res;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        long size = input.nextInt();
+        long[] arr = new long[(int)size];
+        for(int i=0;i<size;i++){
+            arr[i]=input.nextLong();
+        }
+        long inversion = inversionCount(arr,size);
+        
+        System.out.print("Total no of inversion are: "+inversion);
+        
+        input.close();
+    }
+}
