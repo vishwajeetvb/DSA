26e2a58aa9b63504f9457166d83adce58db935a0
Vishwajeet Behera <Vishwajeetbehera0@gmail.com>
2021-06-13 13:59:13 +0530

    Added Singly Linked List All Insertion 4 Types & Deletion

--- dsa/linkedlist/ISLL7LL.java
+++ dsa/linkedlist/ISLL7LL.java
@@ -20,6 +20,68 @@ class ISLL7LL{
       return head;
     }
 
+    static Node insertAtEnd(int value,Node head){
+       System.out.println();
+       Node temp =new Node(value);
+       temp.next=null;
+       Node pointer = head;
+       Node prev = null;
+       while(pointer!=null){
+           prev = pointer;
+           pointer=pointer.next;
+       }
+       prev.next=temp;       
+       return head;
+    }
+
+    static Node insertBeforeGivenValue(int value,int givenValue,Node head){
+        System.out.println();
+        Node temp = new Node(value);
+        Node prev  = null;
+        Node curr = head;
+        while(curr!=null){
+            if(curr.value==givenValue){
+              if(curr==head){
+                temp.next=curr;
+                head=temp;
+                return head;
+              }
+              else{
+                  prev.next=temp;
+                  temp.next=curr;
+                  return head;
+              }
+            }
+            prev=curr;
+            curr=curr.next;
+        }
+        System.out.println("Given Element not Found");
+        return head;
+    }
+
+    static Node insertAfterGivenValue(int value,int givenValue,Node head){
+        System.out.println();
+        Node temp = new Node(value);
+        Node curr = head;
+        while(curr!=null){
+            if(curr.value==givenValue){
+              if(curr==head){
+                temp.next=curr.next;
+                  curr.next=temp;
+                return head;
+              }
+              else{
+                  temp.next=curr.next;
+                  curr.next=temp;
+                  return head;
+              }
+            }
+            curr=curr.next;
+        }
+        System.out.println("Given Element not Found");
+        return head;
+    }
+
     static void display(Node head){
         while(head!=null){
             System.out.print(head.value+"->");;
@@ -32,11 +94,15 @@ class ISLL7LL{
         Node temp=head;;
         Node prev=null;
         while(temp!=null){
+            
             if(temp.value==data){
+                //If The element needed to be deleted is first element
                if(temp==head){
                   head=temp.next;
                   return head;
-               }else{
+               }
+               else//If element is in somewhere middle
+               {
                    prev.next=temp.next;
                    return head;
                } 
@@ -58,6 +124,7 @@ class ISLL7LL{
         head = insertAtStart(18,head);
         head = insertAtStart(19,head);
         head = insertAtStart(17,head);
+        head = insertAtEnd(299, head);
         display(head);
         head = delete(head, 15);
         display(head);
@@ -66,6 +133,26 @@ class ISLL7LL{
         head = delete(head, 17);
         display(head);
         head = delete(head, 14);
+        display(head);
+        head = insertAtEnd(25,head);
+        display(head);
+        head = insertAtEnd(26,head);
+        display(head);
+        head = insertBeforeGivenValue(30, 25, head);
+        display(head);
+        head = insertBeforeGivenValue(1, 19, head);
+        display(head);
+        head = insertBeforeGivenValue(11, 26, head);
+        display(head);
+        head = insertBeforeGivenValue(30, 100, head);
+        display(head);
+        head = insertAfterGivenValue(16, 25, head);
+        display(head);
+        head = insertAfterGivenValue(166, 1, head);
+        display(head);
+        head = insertAfterGivenValue(1666, 26, head);
+        display(head);
+        head = insertAfterGivenValue(16, 250, head);
         display(head);
     }
 }
